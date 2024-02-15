8ad0daf9d27622ca65cfd0819115c0fde4b2a1cf
AbdoMahfouz <7afizooo@gmail.com>
2024-02-15 23:15:17 +0200

    create Note Model

---
+++ lib/model/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String? title;
+  final String? subtitle;
+  final String? data;
+  final int? color;
+
+  NoteModel({
+    this.title,
+    this.subtitle,
+    this.data,
+    this.color,
+  });
+}
