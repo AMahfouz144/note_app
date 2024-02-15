eb5d6db14061647fe9dfe3bb30272acc2c73d95f
AbdoMahfouz <7afizooo@gmail.com>
2024-02-15 23:35:33 +0200

    create hive box

--- lib/model/note_model.dart
+++ lib/model/note_model.dart
@@ -1,13 +1,20 @@
-class NoteModel {
-  final String? title;
-  final String? subtitle;
-  final String? data;
-  final int? color;
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
+  final String title;
+  @HiveField(1)
+  final String subtitle;
+  @HiveField(2)
+  final String data;
+  @HiveField(3)
+  final int color;
 
   NoteModel({
-    this.title,
-    this.subtitle,
-    this.data,
-    this.color,
+    required this.title,
+    required this.subtitle,
+    required this.data,
+    required this.color,
   });
 }
