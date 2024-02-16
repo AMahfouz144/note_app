3dc56443b3de709d9a4dc79462f867ed2592ee2d
AbdoMahfouz <7afizooo@gmail.com>
2024-02-16 02:24:15 +0200

    add cubit

---
+++ lib/cubits/Add/add_note_cubit.dart
@@ -0,0 +1,9 @@
+// ignore: depend_on_referenced_packages
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+part 'add_note_state.dart';
+
+class AddNoteCubit extends Cubit<AddNoteState> {
+  AddNoteCubit() : super(AddNotesInitial());
+}
